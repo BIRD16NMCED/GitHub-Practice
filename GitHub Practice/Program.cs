31112f4e06d69f067bf6ab43857ee23a09673d33
Spencer Bird <bird16@mail.nmc.edu>
2020-09-28 10:46:15 -0400

    initial push

---
+++ GitHub Practice/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+
+
+            age = DisplayGetUserAge();
+
+        }
+
+        static int DisplayGetUserAge()
+        {
+            int age;
+            string userResponse;
+            bool validResponse; 
+
+            //age = Convert.ToInt32(userResponse); for unicorn users, just another way to convert age to int, remember to add validation too for userError checking 
+
+
+            do
+            {
+                validResponse = true;
+
+                Console.Write("Please enter age:");
+                userResponse = Console.ReadLine();
+
+                if (!int.TryParse(userResponse, out age)) //tryparse reverts to int value, checks the value of the number for an integer 
+                {
+                    Console.WriteLine("Please enter a proper integer for your age.");
+                    validResponse = false;
+                }
+
+
+            } while (!validResponse);
+
+            return age; 
+        }
+    }
+}
